467b18b025ea7baeb762c4ca982db36366c94b75
RezaulAlamOni <rzalam93@gmail.com>
2020-11-29 04:21:43 +0600

    image functions

--- image_view.py
+++ image_view.py
@@ -4,16 +4,18 @@ import numpy as np
 # import opencv computer version
 print('package Imported')
 img = cv2.imread('resource/Messi.jpg')
-karnel  = np.ones((5,5),np.uint8)
+karnel = np.ones((5,5),np.uint8)
 
 imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 imageBlur = cv2.GaussianBlur(imgGray,(7,7),0)
 imgCanny = cv2.Canny(img,100,100);
-imaDialation = cv2.dilate(imgCanny,karnel,iterations=1)
+imaDialation = cv2.dilate(imgCanny,karnel,iterations=5)
+imaErode = cv2.erode(imaDialation,karnel,iterations=5)
 
 # iamge view
 cv2.imshow("Messi Output",imgGray)
 cv2.imshow("Messi Blur",imageBlur)
 cv2.imshow("Messi canny",imgCanny)
-cv2.imshow("Messic dialation",imaDialation)
+cv2.imshow("Messi dialation",imaDialation)
+cv2.imshow("Messi imaErode",imaErode)
 cv2.waitKey(0)
